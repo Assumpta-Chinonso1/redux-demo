830780eccf7ab2a1ae577a23156fb48ec9e1446d
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-06-27 12:08:17 +0100

    Redux 4

--- src/index.js
+++ src/index.js
@@ -31,3 +31,13 @@ const reducer = (state = initialState, action) => {
 
 
 const store = creatStore(reducer)
+console.log('initial state', store.getState());
+
+const unsubscribe = store.subscribe(() => console.log('update state', store.getState())
+);
+
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+unsubscribe()
