64d08cbbecc479ef9ec323fdab75e14e6ad25e25
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-06-27 12:02:50 +0100

    Redux 2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+const redux = require('redux')
+const creatStore = redux.creatStore
+
 const CAKE_ORDERED = 'CAKE_ORDERED'
 
 function orderCake() {
     return{
         type: CAKE_ORDERED,
         quantity: 1,
-        
+
     }
     
 }
+
+const initialState = {
+    numberOfCake: 10,
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CAKE_ORDERED:
+            
+        return{
+            ...state,
+            numberOfCake: state.numberOfCake - 1
+        }
+        default:
+            return state
+    }
+}
+
+
+const store = creatStore(reducer)
