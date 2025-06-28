2654645edddbf30ae800a2aa9bf76f321a3da8fb
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-06-28 19:06:50 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ const actions = bindActionCreators({orderCake, restockedCake}, store.dispatch)
 actions.orderCake
 actions.orderCake
 actions.orderCake
-actions.restockedCake(3)
+actions.restockedCake(4)
 
 unsubscribe()
 
