3e09fa454de853afbefd7f691819383bd6f829fd
Celestine Assumpta <assumptachinonso42@gmail.com>
2025-07-01 16:38:22 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import React from 'react'
 
+//store
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>
+        Redux toolkit tutorial
+      </h1>
+    </div>
+  )
+}
+
+export default App
 
 
 
